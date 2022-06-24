e7962b1119a738cd8ee4f9566e720dfa7d3c53a9
louisferrysp <louisferrysp@gmail.com>
2022-06-24 23:24:04 +0700

    add new screen

---
+++ Source_code/lms/lib/screen/kuis_screen.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:checkbox_formfield/checkbox_formfield.dart';
+
+class KuisScreen extends StatefulWidget {
+  const KuisScreen({Key? key}) : super(key: key);
+
+  @override
+  State<KuisScreen> createState() => _KuisScreenState();
+}
+
+class _KuisScreenState extends State<KuisScreen> {
+  void _onChanged(dynamic val) => debugPrint(val.toString());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            ListTile(
+              leading: IconButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, 'detail_course_screen');
+                },
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Colors.black,
+                ),
+              ),
+              title: Text(
+                'Detail Course',
+                style: TextStyle(fontSize: 20),
+              ),
+              trailing: CircleAvatar(
+                backgroundImage: AssetImage('assets/images/landing.png'),
+              ),
+            ),
+            const SizedBox(height: 5),
+            Container(
+              width: 300,
+              child: const Text('Kuis 1 - blablablablablabla'),
+            ),
+            Container(
+              width: 300,
+              child: const Text('(5 soal)'),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              width: double.infinity,
+              height: 441,
+              child: ListView.builder(
+                padding: EdgeInsets.zero,
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return soalCard();
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget checkCard() {
+    return CheckboxListTileFormField(
+      onChanged: _onChanged,
+      title: Text('jawaban salah'),
+    );
+  }
+
+  Widget soalCard() {
+    return Container(
+      padding: EdgeInsets.only(bottom: 20),
+      width: 300,
+      height: 355,
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(8),
+            width: double.infinity,
+            height: 60,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Color.fromRGBO(216, 247, 235, 1),
+            ),
+            child: Text(
+              '1. coba sebutkan kenapa saya membuat ini ?',
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Container(
+            padding: EdgeInsets.all(10),
+            color: Color.fromRGBO(216, 247, 235, 1),
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.zero,
+                  width: double.infinity,
+                  height: 55,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color.fromRGBO(179, 240, 221, 1),
+                  ),
+                  child: checkCard(),
+                ),
+                const SizedBox(height: 5),
+                Container(
+                  padding: EdgeInsets.zero,
+                  width: double.infinity,
+                  height: 55,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color.fromRGBO(179, 240, 221, 1),
+                  ),
+                  child: checkCard(),
+                ),
+                const SizedBox(height: 5),
+                Container(
+                  padding: EdgeInsets.zero,
+                  width: double.infinity,
+                  height: 55,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color.fromRGBO(179, 240, 221, 1),
+                  ),
+                  child: checkCard(),
+                ),
+                const SizedBox(height: 5),
+                Container(
+                  padding: EdgeInsets.zero,
+                  width: double.infinity,
+                  height: 55,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color.fromRGBO(179, 240, 221, 1),
+                  ),
+                  child: checkCard(),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ Source_code/lms/lib/screen/video_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoScreen extends StatefulWidget {
+  const VideoScreen({Key? key}) : super(key: key);
+
+  @override
+  State<VideoScreen> createState() => _VideoScreenState();
+}
+
+class _VideoScreenState extends State<VideoScreen> {
+  var videotitle = 'videotitle';
+
+  VideoPlayerController? _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+        'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4')
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller!.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            ListTile(
+              leading: IconButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, 'detail_course_screen');
+                },
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Colors.black,
+                ),
+              ),
+              title: Text(
+                'Detail Course',
+                style: TextStyle(fontSize: 20),
+              ),
+              trailing: CircleAvatar(
+                backgroundImage: AssetImage('assets/images/landing.png'),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              width: 300,
+              height: 30,
+              child: Text(
+                'Video - $videotitle',
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+            Center(
+              child: _controller!.value.isInitialized
+                  ? AspectRatio(
+                      aspectRatio: _controller!.value.aspectRatio,
+                      child: VideoPlayer(_controller!),
+                    )
+                  : Container(),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _controller!.value.isPlaying
+                ? _controller!.pause()
+                : _controller!.play();
+          });
+        },
+        child: Icon(
+          _controller!.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
+    );
+  }
+}
