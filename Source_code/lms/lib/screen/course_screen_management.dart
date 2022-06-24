ad344565dcaad37c78c753a7214c33d48b3f1b1e
louisferrysp <louisferrysp@gmail.com>
2022-06-24 23:24:44 +0700

    add new route

--- Source_code/lms/lib/screen/course_screen_management.dart
+++ Source_code/lms/lib/screen/course_screen_management.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:lms/screen/course_screen_active.dart';
 import 'package:lms/screen/course_screen_past.dart';
 import 'package:lms/screen/detail_course_screen.dart';
+import 'package:lms/screen/kuis_screen.dart';
+import 'package:lms/screen/video_screen.dart';
 
 class CourseScreen extends StatefulWidget {
   const CourseScreen({Key? key}) : super(key: key);
@@ -13,16 +15,22 @@ class CourseScreen extends StatefulWidget {
 class _CourseScreenState extends State<CourseScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Navigator(
-      onGenerateRoute: (settings) {
-        Widget body = DashBoardCourseActiveScreen();
-        if (settings.name == 'dashboard_past_screen') {
-          body = DashBoardCoursePastScreen();
-        } else if (settings.name == 'detail_course_screen') {
-          body = DetailCourse();
-        }
-        return MaterialPageRoute(builder: (context) => body);
-      },
-    ));
+    return Scaffold(
+      body: Navigator(
+        onGenerateRoute: (settings) {
+          Widget body = DashBoardCourseActiveScreen();
+          if (settings.name == 'dashboard_past_screen') {
+            body = DashBoardCoursePastScreen();
+          } else if (settings.name == 'detail_course_screen') {
+            body = DetailCourse();
+          } else if (settings.name == 'video_screen') {
+            body = VideoScreen();
+          } else if (settings.name == 'kuis_screen') {
+            body = KuisScreen();
+          }
+          return MaterialPageRoute(builder: (context) => body);
+        },
+      ),
+    );
   }
 }
