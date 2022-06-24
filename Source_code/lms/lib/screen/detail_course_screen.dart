ec3e8f813417aacabeaecafbe3b7d0f25236b793
louisferrysp <louisferrysp@gmail.com>
2022-06-24 17:55:37 +0700

    add new screen

--- Source_code/lms/lib/screen/detail_course_screen.dart
+++ Source_code/lms/lib/screen/detail_course_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class DetailCourse extends StatefulWidget {
   const DetailCourse({Key? key}) : super(key: key);
@@ -9,73 +10,132 @@ class DetailCourse extends StatefulWidget {
 }
 
 class _DetailCourseState extends State<DetailCourse> {
+  TextEditingController _ratingController = TextEditingController();
+  var _userRating;
+
   var courseTitle = 'ini Judul course';
   var courseDesc = 'ini deskripsi course';
   var img = 'assets/images/landing.png';
   var role = 'UI UX Designer';
   var name = 'Nama Mentor';
   var progress = '5%';
+  var pembelajaran = '3';
+  bool _isVertical = false;
+
+  IconData? _selectedIcon;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(10),
-        child: Column(
+        child: ListView(
           children: [
-            const SizedBox(height: 10),
-            ListTile(
-              leading: IconButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, 'dashboard_active_screen');
-                },
-                icon: Icon(Icons.arrow_back),
-              ),
-              title: Text(
-                'Detail Course',
-                style: TextStyle(fontSize: 20),
-              ),
-              trailing: CircleAvatar(
-                backgroundImage: AssetImage('assets/images/landing.png'),
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              height: 498,
-              child: Column(
-                children: [
-                  Container(
-                    width: double.infinity,
-                    height: 300,
-                    child: ListView.builder(
-                      padding: EdgeInsets.all(1),
-                      itemCount: 2,
-                      itemBuilder: (context, index) {
-                        return courseCardDetail();
-                      },
+            Column(
+              children: [
+                const SizedBox(height: 10),
+                ListTile(
+                  leading: IconButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, 'dashboard_active_screen');
+                    },
+                    icon: Icon(
+                      Icons.arrow_back,
+                      color: Colors.black,
                     ),
                   ),
-                  const SizedBox(height: 10),
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: const Text(
-                      'Tugas Mendatang :',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  title: Text(
+                    'Detail Course',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  trailing: CircleAvatar(
+                    backgroundImage: AssetImage('assets/images/landing.png'),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 498,
+                  child: Column(
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        height: 300,
+                        child: ListView.builder(
+                          padding: EdgeInsets.all(1),
+                          itemCount: 2,
+                          itemBuilder: (context, index) {
+                            return courseCardDetail();
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Container(
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.play_circle_outline,
+                              color: Colors.black,
+                            ),
+                            const SizedBox(width: 5),
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Text(
+                                '$pembelajaran Pembelajaran  :',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 16),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: 164,
+                        child: ListView.builder(
+                          padding: EdgeInsets.all(1),
+                          itemCount: 2,
+                          itemBuilder: (context, index) {
+                            return excerCard();
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 70),
+                RatingBarIndicator(
+                  rating: _userRating,
+                  itemBuilder: (context, index) => Icon(
+                    _selectedIcon ?? Icons.star,
+                    color: Colors.amber,
+                  ),
+                  itemCount: 5,
+                  itemSize: 50.0,
+                  unratedColor: Colors.amber.withAlpha(50),
+                  direction: _isVertical ? Axis.vertical : Axis.horizontal,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  child: TextFormField(
+                    controller: _ratingController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter rating',
+                      labelText: 'Enter rating',
+                      suffixIcon: MaterialButton(
+                        onPressed: () {
+                          _userRating =
+                              double.parse(_ratingController.text ?? '0.0');
+                          setState(() {});
+                        },
+                        child: Text('Rate'),
+                      ),
                     ),
                   ),
-                  Container(
-                    width: double.infinity,
-                    height: 165,
-                    child: ListView.builder(
-                        padding: EdgeInsets.all(1),
-                        itemCount: 2,
-                        itemBuilder: (context, index) {
-                          return excerCard();
-                        }),
-                  ),
-                ],
-              ),
+                ),
+                const SizedBox(height: 20)
+              ],
             ),
           ],
         ),
