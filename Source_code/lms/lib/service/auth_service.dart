0efcc2b4a69e082cce02c150f15e8f39ffb65b73
louisferrysp <louisferrysp@gmail.com>
2022-07-01 17:11:47 +0700

    update service get user by ID

--- Source_code/lms/lib/service/auth_service.dart
+++ Source_code/lms/lib/service/auth_service.dart
@@ -1,24 +1,24 @@
 import 'dart:convert';
-
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:lms/model/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Auth {
   static Future<dynamic> login(String email, String password) async {
     final dio = Dio();
-    Response response = await dio.post(
-        'https://api-lms-42.herokuapp.com/v1/auth/login',
+    Response response = await dio.post('http://54.254.240.107:4001/v1/login',
         data: {'email': email, 'password': password});
 
     return response.data;
   }
 
-  static Future<Response> getUser(String token) async {
+  Future<dynamic> getUser(var token, var userid) async {
     final dio = Dio();
-    dio.options.headers['content-Type'] = 'application/json';
-    dio.options.headers["bearer"] = "token ${token}";
-    Response response = await dio.get(
-        'https://api-lms-42.herokuapp.com/v1/users/cbac53a6-ecd4-42d4-8b15-555b3aaa9133');
-    return response.data;
+    dio.options.headers['Content-Type'] = 'application/json';
+    dio.options.headers["Authorization"] = "Bearer $token";
+    Response response =
+        await dio.get('http://54.254.240.107:4001/v1/users/$userid');
+    return UserModel.fromJson(response.data);
   }
 }
